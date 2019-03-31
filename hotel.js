bcc221a9e04ed31d54091f05e049a84b697b52bd
Kaung Htet Nyilay <kaunghtetnyilay@outlook.com>
2019-03-31 22:47:07 +0700

    js code

---
+++ hotel.js
@@ -0,0 +1,7 @@
+var mysql = require('mysql');
+var express = require('express');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+//We now need a database to connect to, run the below SQL statement either with command prompt/console or your preferred MySQL Editor.
